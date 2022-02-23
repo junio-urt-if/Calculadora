439102a7ce3c18f9b0a15b9588e2258b184b6904
junio-urt-if <junio.ozn@gmail.com>
2022-02-23 15:01:29 -0300

    springboot person conexao bd

---
+++ 03 CalculadoraRestSpringBoot Person/src/main/java/br/com/ifurt/services/PersonServices.java
@@ -0,0 +1,58 @@
+package br.com.ifurt.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Service;
+
+import br.com.ifurt.model.Person;
+
+@Service
+public class PersonServices {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	public Person create(Person person) {
+		return person;
+	}
+	
+	public Person update(Person person) {
+		return person;
+	}	
+	
+	public void delete(String id) {
+		
+	}
+	
+	public Person findById(String id) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Francisco");
+		person.setLastName("Mineiro");
+		person.setAddress("Maniratuba - Goiás - Brasil");
+		person.setGender("Male");
+		return person;
+	}
+	
+	public List<Person> findAll() {
+		List<Person> persons = new ArrayList<Person>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);			
+		}
+		return persons;
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name" + i);
+		person.setLastName("Last name" + i);
+		person.setAddress("Some address in Brasil" + i);
+		person.setGender("Male");
+		return person;
+	}
+
+}
+
---
+++ 04 CalculadoraRestSpringBoot ConexaoBD/src/test/java/br/com/ifurt/StartupCalculadoraRestSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifurt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupCalculadoraRestSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
